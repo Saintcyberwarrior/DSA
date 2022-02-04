73c57dab82d804d145344dab143bdbee6005e03e
Yash Vardhan Bhobia <yashvardhan@iisc.ac.in>
2022-02-04 16:46:22 +0530

    latest commit

--- Assi_2_yvb/test/test_4.c
+++ Assi_2_yvb/test/test_4.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define gh_kha_lo -1
+
 struct Node{
 	int data;
 	struct Node* next;
@@ -13,22 +15,50 @@ void push(struct Node** a, int new_data){
 	new_node->next = *a;
 	*a = new_node;
 }
-
+/*
 int len(struct Node** a){
-	int count = 0;
-	while((*a)!=NULL){
-	(*a) = (*a)->next;
-	count ++;
+	struct Node * b=(*a);
+	int count = 1;
+	while((b->next)!=NULL){
+		b = b->next;
+		count ++;
 	}
 	return count;
 }
+*/
+int len(struct Node* a){
+	int count =1;
 
-int geti(struct Node* a, int i){
-	int count = 1;
-	for(; i == count; count++){
-		(a) = (*a).next;
+	while((a->next!=NULL)){
+		a = a->next;
+		count++;
 	}
-	return (*a).data;
+
+	return count;
+}
+
+int geti(struct Node** b, int i){
+	struct Node * a = (*b);
+	int inf= 9999;
+	int count = 1;
+	if(i>len2(a)) return gh_kha_lo;
+	for(; i != count; count++){
+		a = a->next;
+	}
+	return a->data;
+}
+
+
+int add(struct Node* a, struct Node* b){
+	int count = 0;
+	len_a = len(a);
+	len_b = len(b);
+	
+	int car = 0;
+	if (count < min(len_a, len_b)){
+	
+	}
+
 }
 
 int main(void){
@@ -41,12 +71,14 @@ int main(void){
 	push(&p, 2);
 
 	printf("%d\n", len(&p));
-	printf("%d\n", geti(p, 1));
-	printf("%d\n", geti(p, 2));
-	printf("%d\n", geti(p, 3));
-	printf("%d\n", geti(p, 4));
-	printf("%d\n", geti(p, 5));
-	printf("%d\n", geti(p, 6));
+//	printf("%d\n", len2(p));
+	printf("%d\n", len(&p));
+	printf("%d\n", geti(&p, 1));
+	printf("%d\n", geti(&p, 2));
+	printf("%d\n", geti(&p, 3));
+	printf("%d\n", geti(&p, 4));
+	printf("%d\n", geti(&p, 5));
+	printf("%d\n", geti(&p, 6));
 	return 0;
 
 }
